9bcbbfd2b2fdb542e94c05cc21474d9b348ff320
jude iwuji <judeiwuji@gmail.com>
2022-08-23 18:31:04 +0100

    Add: Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(0, 10):
+    for j in range(i, 10):
+        end = ", "
+        if i == 9 and j == 9:
+            end = "\n"
+        print("{}{}".format(i, j), end=end)
