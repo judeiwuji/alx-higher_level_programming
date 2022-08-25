e84bbf280b3800240d8cb1c9730547069194d0d2
jude iwuji <judeiwuji@gmail.com>
2022-08-25 14:06:40 +0100

    Fixed error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     args = sys.argv
     argc = len(args)
-    print("{} arguments.".format(argc - 1))
-    
+    msg = "arguments"
+    if argc <= 1:
+        msg = "argument"
+
+    print("{} {}.".format(argc - 1, msg))
     for i in range(1, argc):
         print("{}: {}".format(i, args[i]))
