b8512179852053d4635e479cce38b41f38a2dc33
jude iwuji <judeiwuji@gmail.com>
2022-08-25 15:32:05 +0100

    Fixed bug

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,11 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
+
+def main():
     argv = sys.argv
     argc = len(argv)
-    
+
     if argc < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -25,5 +26,8 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    
     print("{:d} {} {:d} = {:d}".format(a, op, b, result))
+
+
+if __name__ == "__main__":
+    main()
