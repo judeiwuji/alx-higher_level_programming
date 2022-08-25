498027cd9ae079fc682a1c80e3f846db38ee37fa
jude iwuji <judeiwuji@gmail.com>
2022-08-25 14:26:51 +0100

    Bug Fix.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,14 @@ import sys
 if __name__ == "__main__":
     args = sys.argv
     argc = len(args)
-    msg = "arguments"
-    if argc == 2:
-        msg = "argument"
+    msg = ""
+    if argc == 1:
+        msg = "arguments."
+    elif argc == 2:
+        msg = "argument:"
+    else:
+        msg = "arguments:"
 
-    print("{:d} {}.".format(argc - 1, msg))
+    print("{:d} {}".format(argc - 1, msg))
     for i in range(1, argc):
         print("{:d}: {}".format(i, args[i]))
