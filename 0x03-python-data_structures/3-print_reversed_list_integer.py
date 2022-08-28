9c78f24d8088da18123a077ade211f9a2d1de32b
jude iwuji <judeiwuji@gmail.com>
2022-08-28 15:50:11 +0100

    Bug fix: removed reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for el in my_list:
-        print("{:d}".format(el))
+    for i in range(len(my_list) - 1, -1, -1 ):
+        print("{:d}".format(my_list[i]))
