32e90c00913081f03f011cd703700d83fb02875d
jude iwuji <judeiwuji@gmail.com>
2022-08-28 15:27:54 +0100

    Bug fix. pycodestyle

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 
 
 def element_at(my_list, idx):
-    invalid = idx < 0 or len(my_list) < idx
+    invalid = idx < 0 or idx >= len(my_list)
     if invalid:
         return None
     return my_list[idx]
-    
