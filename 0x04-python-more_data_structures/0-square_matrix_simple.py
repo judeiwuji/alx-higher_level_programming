dab65486eb40253ab443a4565ace00b8f2161559
jude iwuji <judeiwuji@gmail.com>
2022-08-31 12:39:49 +0100

    Style Fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 
 
 def square_matrix_simple(matrix=[]):
-    return [[row[x] ** 2 for x in range(len(row)) ] for row in matrix]
+    return [[row[x] ** 2 for x in range(len(row))] for row in matrix]
