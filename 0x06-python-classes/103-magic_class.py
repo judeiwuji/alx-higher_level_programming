bb35df430a1d49e8bb91080c25851f0ca08bf0a8
jude iwuji <judeiwuji@gmail.com>
2022-09-13 20:57:11 +0100

    style fix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import math
 
-
+"""A MagicClass translated from a bytecode."""
 class MagicClass:
     def __init__(self, radius):
+        """Creates a new MagicClass.
+
+        Args:
+            radius: (int)
+        """
         self.radius = 0
 
         if type(radius) is not int:
@@ -12,7 +17,9 @@ class MagicClass:
         self.radius = radius
 
     def area(self):
+        """Computes the area using the radius"""
         return self.radius ** 2 * math.pi
 
     def circumference(self):
+        """Computes the circumference using the radius"""
         return 2 * math.pi * self.radius
