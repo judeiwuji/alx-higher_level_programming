6e835688e50b02303cddba1a8202b30cb345e8c6
jude iwuji <judeiwuji@gmail.com>
2022-09-14 08:22:58 +0100

    updated doc

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-import math
 
 """This module contains a class translated from a bytecode"""
+
+import math
+
+
 class MagicClass:
-    """MagicClass is translated from a bytecodes"""
+    """Translated from a bytecode"""
     def __init__(self, radius):
+        """Instantiate a new object"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
@@ -12,7 +16,9 @@ class MagicClass:
             self.__radius = radius
 
     def area(self):
+        """Computes the area"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Computes the circumference"""
         return 2 * math.pi * self.__radius
