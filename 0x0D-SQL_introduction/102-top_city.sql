055f553f016453cb74e0ec3e2dd758e8d701b6eb
jude iwuji <judeiwuji@gmail.com>
2022-11-02 11:22:34 +0100

    update

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,6 +2,7 @@
 -- and August ordered by temperature (descending)
 SELECT `city`, AVG(`value`) as `avg_temp`
 FROM `temperatures`
-WHERE `month` BETWEEN 7 AND 8
+WHERE `month` IN (7, 8)
+GROUP BY `city`
 ORDER BY AVG(`value`) DESC
 LIMIT 3;
