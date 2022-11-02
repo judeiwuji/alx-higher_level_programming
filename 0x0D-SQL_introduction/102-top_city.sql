98ec6f447a084525caf86ac0aed47fe317d7e1e5
jude iwuji <judeiwuji@gmail.com>
2022-11-02 11:14:04 +0100

    Add: Task 19. Temperatures #1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- displays the top 3 of cities temperature during July
+-- and August ordered by temperature (descending)
+SELECT `city`, AVG(`value`) as `avg_temp`
+FROM `temperatures`
+WHERE `month`=7 or `month`=8
+ORDER BY AVG(`value`) DESC
+LIMIT 3;
