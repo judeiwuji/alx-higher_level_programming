5f5e2d8683f033efd290d6157f4e4e36e91208be
jude iwuji <judeiwuji@gmail.com>
2022-11-02 19:31:04 +0100

    Add: Task 20. Best genre

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT `tv_shows`.`title` AS `title`, SUM(`tv_show_ratings`.`rate`)
-AS `rate`
+AS `rating`
 FROM `tv_show_ratings`
 INNER JOIN `tv_shows`
 ON  `tv_shows`.`id` = `tv_show_ratings`.`show_id`
