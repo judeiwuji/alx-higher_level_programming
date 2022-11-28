24593f2b8b0daad36878a3677b2ea2d592ab4075
jude iwuji <judeiwuji@gmail.com>
2022-11-28 14:56:09 +0100

    Add: Task 10. Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+const result = factorial(num);
+console.log(result);
