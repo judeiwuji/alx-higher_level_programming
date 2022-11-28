ecca3b6a2e459d1bdab0542c79651ee37519590e
jude iwuji <judeiwuji@gmail.com>
2022-11-28 14:41:36 +0100

    Add: Task 8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let row = 0; row < size; row++) {
+    let line = '';
+    for (let col = 0; col < size; col++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
