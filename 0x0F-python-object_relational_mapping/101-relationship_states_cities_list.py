4033bffd3401651b295bb9b3591b1a74f554fc6d
jude iwuji <judeiwuji@gmail.com>
2022-12-02 17:50:33 +0100

    update: use id

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -24,12 +24,10 @@ def main():
     states = session.query(State).join(City).order_by(State.id)\
                                  .order_by(City.id)
 
-    j = 1
-    for i, state in enumerate(states):
-        print("{:d}: {}".format(i+1, state.name))
+    for state in states:
+        print("{:d}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{:d}: {}".format(j, city.name))
-            j += 1
+            print("\t{:d}: {}".format(city.id, city.name))
 
 
 if __name__ == "__main__":
