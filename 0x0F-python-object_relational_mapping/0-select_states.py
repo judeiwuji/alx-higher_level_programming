303ae367541f274dfcc60a73edf3c3e150b287c1
jude iwuji <judeiwuji@gmail.com>
2022-12-02 09:07:27 +0100

    Add: Task 0. Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Executes a SELECT statement using MySQLdb module."""
+import MySQLdb
+import sys
+
+
+def main():
+    """Entry point"""
+    if len(sys.argv) != 4:
+        print("Usage: <username> <password> <database>")
+        return
+
+    DB_HOST = 'localhost'
+    DB_USER = sys.argv[1]
+    DB_PASSWD = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    DB_PORT = 3306
+
+    db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWD,
+                         db=DB_NAME, port=DB_PORT)
+    cur = db.cursor()
+    sql = "SELECT * FROM states ORDER BY id ASC"
+    cur.execute(sql)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()
