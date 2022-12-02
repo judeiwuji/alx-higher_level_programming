1e78c3017f9361f193293d2b867b1f72d26ec89a
jude iwuji <judeiwuji@gmail.com>
2022-12-02 10:04:38 +0100

    Add: Task 4. Cities by states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -28,5 +28,6 @@ def main():
     cur.close()
     db.close()
 
+
 if __name__ == '__main__':
     main()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Executes a SELECT statement using MySQLdb module.
-Selecting only states that start with N
-"""
+"""Executes a SELECT statement using MySQLdb module."""
 import MySQLdb
 import sys
 
 
 def main():
     """Entry point"""
-
     DB_HOST = 'localhost'
     DB_USER = sys.argv[1]
     DB_PASSWD = sys.argv[2]
@@ -18,13 +15,13 @@ def main():
     db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWD,
                          db=DB_NAME, port=DB_PORT)
     cur = db.cursor()
-    sql = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    sql = "SELECT cities.id, cities.name, states.name FROM cities\
+           JOIN states ON states.id=cities.state_id ORDER BY id ASC"
     cur.execute(sql)
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
 
 
 if __name__ == '__main__':
