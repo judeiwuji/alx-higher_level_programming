0fca705ea9cd34b7c304f6eeca452a44f9d9d09c
jude iwuji <judeiwuji@gmail.com>
2022-12-16 17:17:53 +0100

    Add: Task 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+from urllib.request import Request
+from urllib.request import urlopen
+
+def main():
+    """Main: function"""
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+
+if __name__ == "__main__":
+    main()
