0e8abc493587f75a7fd17197eac469532fa8b2fb
jude iwuji <judeiwuji@gmail.com>
2022-12-16 09:24:56 +0100

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # takes in a URL, sends a request to that URL, and displays
-# the size of the body of the response
-curl --silent "$1" | wc --bytes
+curl -s "$1" | wc --bytes
