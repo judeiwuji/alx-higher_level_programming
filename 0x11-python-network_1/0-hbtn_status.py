00f7a58c593adb1353d2012c4d2b7f76c1686c22
jude iwuji <judeiwuji@gmail.com>
2022-12-16 17:19:22 +0100

    fixed style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,7 @@
 from urllib.request import Request
 from urllib.request import urlopen
 
+
 def main():
     """Main: function"""
     req = Request("https://alx-intranet.hbtn.io/status")
@@ -12,6 +13,7 @@ def main():
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode("utf-8")))
+
 
 if __name__ == "__main__":
     main()
