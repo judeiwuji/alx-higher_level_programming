9c618fe3fecdcf2366481cb7e937381763f9a762
jude iwuji <judeiwuji@gmail.com>
2023-01-17 05:14:49 -0600

    Add: Task 4

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,9 @@ const request = require('request');
 
 const url = process.argv[2];
 request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log('code', response && response.statusCode);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,10 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
   const data = JSON.parse(body);
   console.log(data.title);
 });
