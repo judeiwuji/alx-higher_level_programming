b551783a5a1f099a7bbc360994126f94f054582a
jude iwuji <judeiwuji@gmail.com>
2023-01-17 05:01:17 -0600

    Add: Task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+request(url, function (error, response, body) {
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
